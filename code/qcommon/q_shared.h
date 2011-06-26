ffe5984c882e043ad9dc314e3e47d2d4d21dff41
DaTa <poddata@ukr.net>
2011-06-27 02:11:31 +0300

    Avoiding server browser confusion by changing Q3_VERSION format.

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // q_shared.h -- included first by ALL program modules.
 // A user mod should never modify this file
 
-#define Q3_VERSION				"CNQ3X 1.47.1x"
+#define Q3_VERSION				"CNQ3 X 1.47.1x"
 #define CLIENT_WINDOW_TITLE		"CNQ3X"
 #define CONSOLE_WINDOW_TITLE	"CNQ3X Console"
 
